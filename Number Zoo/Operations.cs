0ba2576830dab8a814d81af2cbf0dc36d5eb5544
MrMorham <g7058190@gmail.com>
2019-01-30 20:14:00 +0000

    Added Feature - Generate Zoo and remove number

---
+++ Number Zoo/Operations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Zoo
+{
+    class Operations
+    {
+        public List<int> GenerateZoo (int size)
+        {
+            List<int> Zoo = new List<int>();
+
+            FillZoo(Zoo, size);
+            ReleaseNumber(Zoo, size);
+
+            return Zoo;
+        }
+
+        private void FillZoo (List<int> zoo, int size)
+        {
+            for(int i = 1; i <= size; i++)
+            {
+                zoo.Add(i);
+            }
+        }
+
+        private void ReleaseNumber (List<int> zoo, int size)
+        {
+            Random random = new Random();
+
+            int escapee = random.Next(1, size);
+
+            zoo.Remove(escapee);
+        }
+
+        public void PrintZoo (List<int> zoo)
+        {
+            foreach (int item in zoo)
+            {
+                Console.Write(item + "|");
+            }
+        }
+
+
+
+    }
+}
---
+++ Number Zoo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+You're working in a number zoo, and it seems that one of the numbers has gone missing!
+
+Zoo workers have no idea what number is missing, and are too incompetent to figure it out, so they're hiring you to do it for them.
+
+In case the zoo loses another number, they want your program to work regardless of how many numbers there are in total.
+
+Task:
+Write a function that takes a shuffled array of unique numbers from 1 to n with one element missing (which can be any number including n). Return this missing number.
+
+Examples:
+Kata.FindNumber(new int[] {1, 3, 4}) => 2
+Kata.FindNumber(new int[] {1, 2, 3}) => 4
+Kata.FindNumber(new int[] {4, 2, 3}) => 1
+*/
+
+namespace Number_Zoo
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Operations Command = new Operations();
+
+            List<int> Zoo = Command.GenerateZoo(10);
+
+            Command.PrintZoo(Zoo);
+
+            Console.ReadLine();
+        }
+    }
+}
